641788791c9cfe6097c0e72b04869e546e01d895
John Dale <unitejohndalev@gmail.com>
2023-06-24 20:03:37 +0800

    added src files and install dependencies/packages

---
+++ src/components/NewItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewItems = () => {
+  return (
+    <div>NewItems</div>
+  )
+}
+
+export default NewItems
---
+++ src/components/PorductSlider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PorductSlider = () => {
+  return (
+    <div>PorductSlider</div>
+  )
+}
+
+export default PorductSlider
